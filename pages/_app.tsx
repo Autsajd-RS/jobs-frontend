f567c391e5dea6b910748dcb8332b0e9ac4ce871
Boris <boris.evetovic777@gmail.com>
2022-01-30 17:19:26 +0100

    Added material ui, configured emotion caching on SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import createEmotionCache from '../utility/createEmotionCache';
+import { EmotionCache } from '@emotion/cache';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import lightTheme from '../styles/theme/lightTheme';
+import { CssBaseline } from '@mui/material';
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props: MyAppProps) => {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
+};
+
+export default MyApp;
